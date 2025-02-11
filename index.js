ee4e5e4687c8e87b2470af7186336c2db5afbff4
vaishnavi640 <vaishnavikalluri@gmail.com>
2025-02-11 11:55:17 +0530

    made changes in index.js file

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
+const isAdmin = process.env.IS_ADMIN === 'true';
+
+if (isAdmin) {
+  console.log("Admin privileges granted.");
+} else {
+  console.log("Access restricted. Admin only.");
+}const express = require('express');
 require('dotenv').config();
-const express = require('express');
-const { resolve } = require('path');
 
 const app = express();
-const port = 3010;
-
-app.use(express.static('static'));
-
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
-
-
 
 app.get('/', (req, res) => {
   if (process.env.IS_ADMIN === 'true') {
@@ -25,5 +16,4 @@ app.get('/', (req, res) => {
     res.send({ message: "Welcome, User!", data: ["User Data 1", "User Data 2"] });
   }
 });
-
 app.listen(3000, () => console.log("Server is running on port 3000."));
